9d5b71a1c624ff8395394448ebfaf5fec50f4633
Dave Wilson <parttimehacker@gmail.com>
2024-02-25 09:40:43 -0800

    Update update_script.sh

--- update_script.sh
+++ update_script.sh
@@ -5,3 +5,5 @@ sudo cp $1.py /usr/local/$1
 sudo cp ./logging.ini /usr/local/$1
 sudo cp -r ./pkg_classes /usr/local/$1
 sudo cp $1.service /lib/systemd/system/$1.service
+sudo systemctl daemon-reload
+sudo systemctl enable $1.service
